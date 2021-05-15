58dbab822a41246f85a3eb27e445da4e177775e3
mzaeemz <zaeemzahid98@gmail.com>
2021-05-15 21:30:05 +0500

    Added Unstaged Files

---
+++ precache-manifest.db02d1de587f381122b49d6e82ef6bfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b4c778db7ee18269ff21de2dfa730d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf4ca79dfc0168b716e0",
+    "url": "/static/css/main.eae65632.chunk.css"
+  },
+  {
+    "revision": "cbe964fde2277da60b5d",
+    "url": "/static/js/2.5942a445.chunk.js"
+  },
+  {
+    "revision": "bf4ca79dfc0168b716e0",
+    "url": "/static/js/main.0ecd266c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
